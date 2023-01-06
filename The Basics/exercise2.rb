1abae2cca99a2dc4fd0329521dc85f4117c6ad62
David Wolinsky <david.wolinsky@venturetec.net>
2023-01-05 19:04:01 -0500

    finsihed a few exercises

---
+++ The Basics/exercise2.rb
@@ -0,0 +1,8 @@
+thousands = 3776 / 1000
+hundreds = 3776 % 1000 / 100
+tens = 3776 % 100 / 10
+ones = 3776 % 10
+puts "thousands: #{thousands}"
+puts "hundreds: #{hundreds}"
+puts "tens: #{tens}"
+puts "ones: #{ones}"
